8756967f6d2316349307b56dbf7a4e59c7a019d6
sql <teshyfraud@gmail.com>
2025-06-27 00:23:18 -0400

    Update revixpopup.js

--- revixpopup.js
+++ revixpopup.js
@@ -108,7 +108,7 @@
   card.className = 'revix-card';
   card.innerHTML = `
     <div class="revix-logo">
-      <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" style="width:48px;height:48px;fill:white;"><path d="M24.3,6A11.2,11.2,0,0,0,16,9.3a11,11,0,0,0-3.5,8.2,2.5,2.5,0,0,0,5,0,6.5,6.5,0,0,1,2-4.7A6.2,6.2,0,0,1,24.2,11a6.5,6.5,0,0,1,1,12.9,4.4,4.4,0,0,0-3.7,4.4v3.2a2.5,2.5,0,0,0,5,0V28.7a11.6,11.6,0,0,0,9-11.5A11.7,11.7,0,0,0,24.3,6Z"/><circle cx="24" cy="39.5" r="2.5"/></svg>
+      <img src="icon.png" style="height:48px;width:48px;" alt="Revix">
       <div>Revix</div>
     </div>
     <div class="revix-section">Version <span class="revix-badge version">v2.5</span></div>
